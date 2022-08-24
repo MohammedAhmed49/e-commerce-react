8adee86ef9be8c335bad7a5377b96d63cb4ad801
Mohammed Ahmed <mohammed_ahmed4949@yahoo.com>
2022-08-24 08:18:37 +0200

    Uploaded all categories and products data once on firestore using writeBatch(), and then fetching all the categories and the product data.

--- src/context/products.context.jsx
+++ src/context/products.context.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import PRODUCTS from '../shop-data.json';
+import { addCollectionAndDocuments, getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
 
 export const ProductsContext = createContext({
     products: [],
@@ -9,6 +10,15 @@ export const ProductsContext = createContext({
 export const ProductsContextProvider = ({ children }) => {
     const [products, setProducts] = useState(PRODUCTS);
     const value = {products, setProducts};
+
+    useEffect(() => {
+        const getData = async () => {
+            const categoriesMap = await getCategoriesAndDocuments();
+            console.log(categoriesMap);
+        }
+
+        getData();
+    }, [])
 
     return (
         <ProductsContext.Provider value={value}>
