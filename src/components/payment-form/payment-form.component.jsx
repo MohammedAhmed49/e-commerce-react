9bbe69e7182f63f3a8444ee9d3078851787c5bbd
Mohammed Ahmed <mohammed_ahmed4949@yahoo.com>
2022-09-05 23:50:03 +0200

    Created  the payment handler function to confirm payment.

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,4 +1,3 @@
-import { async } from "@firebase/util";
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import Button from "../button/button.component";
 import './payment-form.styles.scss';
@@ -11,14 +10,42 @@ const PaymentForm = () => {
     const paymentHandler = async (e) => {
         e.preventDefault();
         if( !stripe || !elements ) return;
+
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ amount: 1000 })
+        }).then(res => res.json());
+
+        const clientSecret = response.paymentIntent.client_secret;
+
+
+        const payment = await stripe.confirmCardPayment(clientSecret,{
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: 'Mohammed Ahmed'
+                }
+            }
+        });
+
+        if (payment.error) {
+            alert(payment.error)
+        } else {
+            if( payment.paymentIntent.status === 'succeeded' ) {
+                alert('Payment Succeeded');
+            }
+        }
     }
     return(
         <div className="payment-form-container">
-            <div className="form-container">
+            <form className="form-container" onSubmit={paymentHandler}>
                 <h2>Credit Card Payment: </h2>
                 <CardElement />
                 <Button buttonType='inverted'>Pay Now</Button>
-            </div>
+            </form>
         </div>
     )
 }
