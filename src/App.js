dadaf0f9428166e5b90a43b8c5086cb97160cb64
Mohammed Ahmed <mohammed_ahmed4949@yahoo.com>
2022-09-06 07:18:10 +0200

    Some performance enhancement by applying lazy loading.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import Home from "./routes/home/home.component";
 import {
   Routes,
   Route
 } from "react-router-dom";
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/auth/auth.component";
-import Shop from "./routes/shop/shop.component";
-import Checkout from "./routes/checkout/checkout.component";
-import Category from "./routes/category/category.component";
-import { useEffect } from "react";
+import { lazy, Suspense, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchCategoriesStart } from "./store/categories/categories.actions";
 import { checkUserSession } from "./store/user/user.actions";
+import Spinner from "./components/spinner/spinner.component";
+
+const Home = lazy(() => import('./routes/home/home.component'));
+const Navigation = lazy(() => import('./routes/navigation/navigation.component'));
+const Authentication = lazy(() => import('./routes/auth/auth.component'));
+const Shop = lazy(() => import('./routes/shop/shop.component'));
+const Category = lazy(() => import('./routes/category/category.component'));
+const Checkout = lazy(() => import('./routes/checkout/checkout.component'));
 
 
 const App = () => {
@@ -27,17 +29,19 @@ const App = () => {
   }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop">
-          <Route index element={<Shop />}/>
-          <Route path=":cateName" element={<Category />}/>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop">
+            <Route index element={<Shop />}/>
+            <Route path=":cateName" element={<Category />}/>
+          </Route>
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
         </Route>
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+      </Routes>
+    </Suspense>
   )
 }
 
